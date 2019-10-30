8c66099eebd0bc025b081f3bc43a697749c627f0
Chris Dickinson <christopher.s.dickinson@gmail.com>
2019-10-30 10:14:50 -0700

    initial code commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# git-the-latest
+
+`npx git-the-latest`
+
+This configures `git init` to default the primary branch to `latest` instead of
+`master` by creating a git template directory at `~/.git-template` and running
+`git config --global` to set that directory as the default for creating new
+repos.
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+const { spawnSync, execSync } = require('child_process')
+const untildify = require('untildify')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+
+const dir = path.join(os.homedir(), '.git-template')
+if (fs.existsSync(dir)) {
+  console.error('Template already exists. Run "rm -rf ~/.git-template" and re-run to upgrade.')
+  return process.exit(1)
+}
+
+const { stdout } = spawnSync('mktemp', ['-d'])
+const tmp = String(stdout).trim()
+
+spawnSync('git', ['init'], {
+  cwd: tmp
+})
+
+fs.writeFileSync(path.join(tmp, '.git', 'HEAD'), 'ref: refs/heads/latest')
+fs.renameSync(path.join(tmp, '.git'), dir)
+fs.rmdirSync(tmp)
+
+spawnSync('git', ['config', '--global', '--unset-all', 'init.templateDir'])
+spawnSync('git', ['config', '--global', '--add', 'init.templateDir', dir])
